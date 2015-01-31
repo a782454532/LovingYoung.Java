44f5448f5da696d8d48be11508c87a9affebc4e0
LovingYoung <782454532@qq.com>
2015-01-31 13:23:47 +0800

    import

---
+++ Chapter05/src/Apple.java
@@ -0,0 +1,7 @@
+package lovingyoung.apple;
+public class Apple{
+	public static void main(String[] args){
+		System.out.println("Apple");
+	}
+}
+
---
+++ Chapter05/src/HelloTest.java
@@ -0,0 +1,8 @@
+package lovingyoung;
+import lovingyoung.apple.Apple;
+public class HelloTest{
+	public static void main(String[] args){
+		Hello.main(args);
+		Apple app=new Apple();
+	}
+}
